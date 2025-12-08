0f80f8caab8565d9f991d921048ea55fb5f1bfcf
Julie Krasnick <julie.krasnick@viam.com>
2025-12-08 09:31:56 -0500

    consts

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,9 +1,9 @@
 class Consts {
-  static const String hotspotPrefix = 'gost';
-  static const String hotspotPassword = 'GOSTadm1n';
+  static const String hotspotPrefix = '';
+  static const String hotspotPassword = '';
 
-  static const String apiKeyId = 'd551ed08-c9d7-4535-becc-b3db7d36a162';
-  static const String apiKey = 'lljkf80uv4y3srkzj04y9y2y3tar84dz';
+  static const String apiKeyId = '';
+  static const String apiKey = '';
 
-  static const String organizationId = '782a4050-a135-4e19-ad5e-d98b33ed7309';
+  static const String organizationId = '';
 }
